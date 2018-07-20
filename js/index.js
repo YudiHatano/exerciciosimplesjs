f1b7861aaac6bce66cdda37c07ab290cb71827b5
YudiHatano <istivijapa@hotmail.com>
2018-07-20 14:21:42 -0300

    Comentar o código JS

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
-//Declaração de 
+//Declaração de variaveis:
 var primeiro = parseInt(prompt("Informe o primeiro valor: "));
 var segundo = parseInt(prompt("Informe o segundo valor: "));
 var escolha;
 
-// Menu de Opções
+// Menu de Opções:
 escolha = prompt("A. Soma de dois números" + "\n" +
                  "B. Diferença entre dois números" + "\n" +
                  "C. Produto entre dois números" + "\n" +
                  "D. Divisão entre dois números" + "\n" + 
                  "Escolha uma opção em número: ").toUpperCase();
 
-// Casos do Switch
+// Casos do Switch:
+  //Soma:
 switch(escolha) {
   case "A":
     alert((primeiro) + (segundo));
     break;
-    
+
+  //Subtracao:
   case "B":
     if (primeiro > segundo){
        alert((primeiro) - (segundo));
@@ -23,11 +25,13 @@ switch(escolha) {
         alert((segundo) - (primeiro));
         }
     break;
-    
+
+  //Multiplicacao:
   case "C":
     alert((primeiro) * (segundo));
     break;
     
+  //Divisao:
   case "D":
     if (primeiro > segundo){
       alert((primeiro) / (segundo));
@@ -35,7 +39,8 @@ switch(escolha) {
        alert((segundo) / (primeiro));
        }
     break;
-    
+
+  //Caso receba qualquer outra resposta:
   default:
     alert("A opção digitada não é válida!");
     break;
