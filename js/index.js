95731c9436d74f78305f5648fa6b1c9ff9d8e334
YudiHatano <istivijapa@hotmail.com>
2018-07-20 13:13:35 -0300

    Adicionar exercicio js

---
+++ js/index.js
@@ -0,0 +1,42 @@
+//Declaração de 
+var primeiro = parseInt(prompt("Informe o primeiro valor: "));
+var segundo = parseInt(prompt("Informe o segundo valor: "));
+var escolha;
+
+// Menu de Opções
+escolha = prompt("A. Soma de dois números" + "\n" +
+                 "B. Diferença entre dois números" + "\n" +
+                 "C. Produto entre dois números" + "\n" +
+                 "D. Divisão entre dois números" + "\n\n" + 
+                 "Escolha uma opção em número: ").toUpperCase();
+
+// Casos do Switch
+switch(escolha) {
+  case "A":
+    alert((primeiro) + (segundo));
+    break;
+    
+  case "B":
+    if (primeiro > segundo){
+       alert((primeiro) - (segundo));
+    } else {
+        alert((segundo) - (primeiro));
+        }
+    break;
+    
+  case "C":
+    alert((primeiro) * (segundo));
+    break;
+    
+  case "D":
+    if (primeiro > segundo){
+      alert((primeiro) / (segundo));
+    } else{
+       alert((segundo) / (primeiro));
+       }
+    break;
+    
+  default:
+    alert("A opção digitada não é válida!");
+    break;
+}
